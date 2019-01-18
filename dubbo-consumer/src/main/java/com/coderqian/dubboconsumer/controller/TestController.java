d7aa5efda1d3a05d950da550fee97d9af971d4f1
qianlq <qianliqing@hyperchain.cn>
2019-01-18 16:48:08 +0800

    :octocat: feat(Hystrix熔断器): 初步继承Hystrix熔断器

--- dubbo-consumer/src/main/java/com/coderqian/dubboconsumer/controller/TestController.java
+++ dubbo-consumer/src/main/java/com/coderqian/dubboconsumer/controller/TestController.java
@@ -2,6 +2,7 @@ package com.coderqian.dubboconsumer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.coderqian.dubboapi.service.TestService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author qianliqing
  * @date 2018/11/29 1:52 PM
- * email: qianliqing@hyperchain.com
+ * email: qianlq0824@gmail.com
  */
 
 @RestController
@@ -19,8 +20,13 @@ public class TestController {
     @Reference
     private TestService testService;
 
+    @HystrixCommand(fallbackMethod = "testError")
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String test(String text) {
         return testService.test(text);
+    }
+
+    private String testError(String text) {
+        return "失败" + text;
     }
 }
--- dubbo-provider/src/main/java/com/coderqian/dubboprovider/service/impl/TestServiceImpl.java
+++ dubbo-provider/src/main/java/com/coderqian/dubboprovider/service/impl/TestServiceImpl.java
@@ -2,18 +2,25 @@ package com.coderqian.dubboprovider.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.coderqian.dubboapi.service.TestService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import io.swagger.annotations.ApiParam;
 
 /**
  * @author qianliqing
  * @date 2018/11/29 12:53 PM
- * email: qianliqing@hyperchain.com
+ * email: qianlq0824@gmail.com
  */
 
 @Service
 public class TestServiceImpl implements TestService {
 
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "3"),
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    })
     @Override
-    public String test(String text) {
+    public String test(@ApiParam(value = "测试数据", required = true) String text) {
         return text;
     }
 }
