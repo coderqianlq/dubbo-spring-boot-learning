e0a34cae1c4e059ab460262af8984ece992a52a6
qianlq <qianliqing@hyperchain.cn>
2019-01-22 12:57:39 +0800

    feat(工具类模块): 添加工具类模块

---
+++ dubbo-common/src/test/java/com/coderqian/dubbocommon/DubboCommonApplicationTests.java
@@ -0,0 +1,17 @@
+package com.coderqian.dubbocommon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DubboCommonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
